31efdab5bc1a5852101f46ec0172ed27f6840830
MohdAreef <145595084+MohdAreef@users.noreply.github.com>
2025-03-04 12:12:58 +0530

    Update unique_paths.java
    Space Optimization

--- Dynamic Programming/2D_3D_Grids_DP/unique_paths.java
+++ Dynamic Programming/2D_3D_Grids_DP/unique_paths.java
@@ -1,5 +1,4 @@
 // TABULATION
-
 class Solution {
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[m][n];
@@ -27,6 +26,55 @@ class Solution {
             }
         }
         return dp[m - 1][n - 1];
+
+    }
+}
+
+
+// SPACE OPTIMIZATION
+class Solution {
+    public int uniquePaths(int m, int n) {
+        // int[][] dp = new int[m][n];
+
+        int[] prevrow = new int[n];
+        for (int i = 0; i < n; i++)
+            prevrow[i] = 1;
+
+        // int[] currow=new int[n];
+        // currrow[0]=1;
+
+        // for (int i = 0; i < m; i++)
+        // dp[i][0] = 1;
+
+        // for (int j = 0; j < n; j++)
+        // dp[0][j] = 1;
+
+        int[] rows = new int[] { -1, 0 };
+        int[] cols = new int[] { 0, -1 };
+
+        for (int row = 1; row < m; row++) {
+            int[] currrow = new int[n];
+            currrow[0] = 1;
+            for (int col = 1; col < n; col++) {
+                int sum = 0;
+                // for (int i = 0; i < 2; i++) {
+                // int newrow = row + rows[i];
+                // int newcol = col + cols[i];
+                // if (newrow >= 0 && newrow < m && newcol >= 0 && newcol < n) {
+                // sum += dp[newrow][newcol];
+                // }
+                // }
+
+                sum = prevrow[col] + currrow[col - 1];
+                currrow[col] = sum;
+
+                // dp[row][col] = sum;
+            }
+
+            prevrow = currrow;
+            currrow = null;
+        }
+        return prevrow[n - 1];
 
     }
 }
