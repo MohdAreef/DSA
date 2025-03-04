ee29ee04327a9fa3baf4ba7c5bb9cb46e0dee43a
MohdAreef <145595084+MohdAreef@users.noreply.github.com>
2025-03-04 10:43:38 +0530

    Update ninja's_training.java
    Space Optimization

--- Dynamic Programming/2D_3D_Grids_DP/ninja's_training.java
+++ Dynamic Programming/2D_3D_Grids_DP/ninja's_training.java
@@ -40,3 +40,49 @@ class Solution {
      
     }
 }
+
+// SPACE OPTIMIZATION
+class Solution {
+    public int maximumPoints(int arr[][]) {
+        // code here
+    int n=arr.length;
+    int[][] dp=new int[n][3];
+    int[] prev=new int[3];
+    for(int j=0;j<3;j++)
+    {
+        int maxi=0;
+        for(int i=0;i<3;i++)
+        {
+            if(i!=j)
+            {
+                maxi=Math.max(maxi,arr[0][i]);
+            }
+        }   
+        // dp[0][j]=maxi;
+        prev[j]=maxi;
+    }
+        for(int day=1;day<n;day++)
+        {
+            int[] curr=new int[3];
+            
+            for(int last=0;last<3;last++)
+            {
+                int maxpoints=0;
+                
+                for(int i=0;i<3;i++)
+                {
+                    if(i!=last)
+                    {
+                        // maxpoints=Math.max(maxpoints,dp[day-1][i]+arr[day][i]);
+                        maxpoints=Math.max(maxpoints,prev[i]+arr[day][i]);
+                    }
+                }
+                curr[last]=maxpoints;      
+            }
+                prev=curr;
+                curr=null;
+        }
+        // return Math.max(dp[n-1][0],Math.max(dp[n-1][1],dp[n-1][2]));
+        return Math.max(prev[0],Math.max(prev[1],prev[2]));
+    }
+}
