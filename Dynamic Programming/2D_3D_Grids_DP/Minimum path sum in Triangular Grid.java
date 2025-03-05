beb75b0e02f877a2192a628f97e604a4353ddfe9
MohdAreef <145595084+MohdAreef@users.noreply.github.com>
2025-03-05 12:29:41 +0530

    Update Minimum path sum in Triangular Grid.java

--- Dynamic Programming/2D_3D_Grids_DP/Minimum path sum in Triangular Grid.java
+++ Dynamic Programming/2D_3D_Grids_DP/Minimum path sum in Triangular Grid.java
@@ -31,3 +31,39 @@ class Solution {
 
     }
 }
+// SPACE OPTIMIZATION
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+
+        List<Integer> prev = new ArrayList<>();
+        prev.add(triangle.get(0).get(0));
+
+        int n = triangle.size();
+
+        for (int level = 1; level < n; level++) {
+            List<Integer> curr = new ArrayList<>();
+            for (int index = 0; index < triangle.get(level).size(); index++) {
+
+                int left = Integer.MAX_VALUE;
+                int right = Integer.MAX_VALUE;
+
+                if ((index - 1) >= 0)
+                    left = triangle.get(level).get(index) + prev.get(index - 1);
+
+                if (index < triangle.get(level - 1).size())
+                    right = triangle.get(level).get(index) + prev.get(index);
+
+                curr.add(Math.min(left, right));
+
+            }
+            prev = curr;
+            curr = null;
+        }
+        int ans = Integer.MAX_VALUE;
+        for (int i = 0; i < prev.size(); i++) {
+            ans = Math.min(ans, prev.get(i));
+        }
+        return ans;
+
+    }
+}
