2fba5826c26c1920c364dae740742078daf931e9
MohdAreef <145595084+MohdAreef@users.noreply.github.com>
2025-03-08 17:36:41 +0530

    Update Coin Change 2 (DP - 22).java

--- Dynamic Programming/ DP on Subsequences/Coin Change 2 (DP - 22).java
+++ Dynamic Programming/ DP on Subsequences/Coin Change 2 (DP - 22).java
@@ -6,6 +6,7 @@ You may assume that you have an infinite number of each kind of coin.
 
 The answer is guaranteed to fit into a signed 32-bit integer.
 */
+// MEMOIZATION
 class Solution {
     public int totalcombo(int index, int target, int[] coins, int[][] dp) {
 
@@ -36,5 +37,64 @@ class Solution {
             Arrays.fill(i, -1);
         }
         return totalcombo(n - 1, amount, coins, dp);
+    }
+}
+// TABULATION
+class Solution {
+    public int change(int amount, int[] coins) {
+        int n = coins.length;
+        int[][] dp = new int[n][amount + 1];
+        for (int amt = 0; amt <= amount; amt++) {
+            if (amt % coins[0] == 0)
+                dp[0][amt] = 1;
+            else
+                dp[0][amt] = 0;
+        }
+
+        for (int index = 1; index < n; index++) {
+
+            for (int amt = 0; amt <= amount; amt++) {
+                int pick = 0;
+                if (coins[index] <= amt)
+                    pick = dp[index][amt - coins[index]];
+
+                int notpick = dp[index - 1][amt];
+
+                dp[index][amt] = pick + notpick;
+            }
+        }
+        return dp[n - 1][amount];
+    }
+}
+
+// SPACE OPTIMIZATION
+class Solution {
+    public int change(int amount, int[] coins) {
+        int n = coins.length;
+
+        int[] prev = new int[amount + 1];
+
+        for (int amt = 0; amt <= amount; amt++) {
+            if (amt % coins[0] == 0)
+                prev[amt] = 1;
+            else
+                prev[amt] = 0;
+        }
+
+        for (int index = 1; index < n; index++) {
+            int[] curr = new int[amount + 1];
+            for (int amt = 0; amt <= amount; amt++) {
+                int pick = 0;
+                if (coins[index] <= amt)
+                    pick = curr[amt - coins[index]];
+
+                int notpick = prev[amt];
+
+                curr[amt] = pick + notpick;
+            }
+            prev = curr;
+            curr = null;
+        }
+        return prev[amount];
     }
 }
