5dbc6e90a233f89c2090c6f62e5e1cb3fe24ef84
MohdAreef <145595084+MohdAreef@users.noreply.github.com>
2025-03-08 13:42:17 +0530

    Update Count Partitions with Given Difference (DP - 18).java
    TABULATION SOLUTION

--- Dynamic Programming/ DP on Subsequences/Count Partitions with Given Difference (DP - 18).java
+++ Dynamic Programming/ DP on Subsequences/Count Partitions with Given Difference (DP - 18).java
@@ -4,7 +4,7 @@ of these two subsets be S1 and S2.
 Given a difference d, count the number of partitions in which S1 is greater than or equal to S2 and the difference between S1 and S2 is equal to d.
 Since the answer may be large return it modulo 109 + 7.
 */
-
+// MEMOIZATION
 class Solution {
     public static int count(int index,int sum,int[] arr,int[][] dp)
     {
@@ -57,4 +57,50 @@ class Solution {
             return count(n-1,k,arr,dp)% (int) (1e9+7);
     }
 }
+
+// TABULATION 
+class Solution {
+    int countPartitions(int[] arr, int d) {
+        // code here
+       int totalsum=0,n=arr.length;
+       
+       for(int i=0;i<n;i++)
+       {
+           totalsum+=arr[i];
+       }
+       
+       if((d+totalsum) % 2 !=0)
+       return 0;
+       
+       int s1=(d+totalsum)/2;
+       int s2=totalsum-s1;
+       if(s1<s2) return 0;
+       
+       int tar=s1;
+       
+       int[][] dp=new int[n][s1+1];
+       
+       if(arr[0]==0) dp[0][0]=2;
+       else dp[0][0]=1;
+       
+       // arr[0] = 0
+       if(arr[0]!=0 && arr[0]<=tar) dp[0][arr[0]]=1;
+       
+       for(int index=1;index<n;index++)
+       {
+           for(int target=0;target<=tar;target++)
+           {
+                int pick=0,notpick=0;
+           if(arr[index]<=target)
+             pick=dp[index-1][target-arr[index]];
+        
+             notpick=dp[index-1][target];
+           
+           dp[index][target]=pick+notpick;
+           }
+       }
+       return dp[n-1][tar];
+    }
+}
+
         
