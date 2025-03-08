4e1cb5f8876ab5e5158126407957bfad4527c216
MohdAreef <145595084+MohdAreef@users.noreply.github.com>
2025-03-08 19:12:15 +0530

    Update Unbounded Knapsack (DP - 23).java
    space optimization

--- Dynamic Programming/ DP on Subsequences/Unbounded Knapsack (DP - 23).java
+++ Dynamic Programming/ DP on Subsequences/Unbounded Knapsack (DP - 23).java
@@ -4,6 +4,7 @@ Given a set of N items, each with a weight and a value, represented by the array
 The task is to fill the knapsack in such a way that we can get the maximum profit. Return the maximum profit.
 Note: Each item can be taken any number of times. 
 */
+// MEMOIZATION
 class Solution{
     static int maxprofit(int index,int weight,int[] val,int[] wt,int[][] dp)
     {
@@ -41,5 +42,71 @@ class Solution{
        int res=maxprofit(N-1,W,val,wt,dp);
        if(res==(int)-1e9) return 0;
        return res;
+    }
+}
+// TABULATION
+class Solution {
+    static int knapSack(int val[], int wt[], int capacity) {
+        // code here
+        int n=val.length;
+        int[][] dp=new int[n][capacity+1];
+        
+        for(int cap=0;cap<=capacity;cap++)
+        {
+                dp[0][cap]=(cap/wt[0]) * val[0];
+        }
+        
+        for(int index=1;index<n;index++)
+        {
+            for(int cap=0;cap<=capacity;cap++)
+            {
+                int pick=(int)-1e9;
+                if(wt[index] <=cap)
+                {
+                    pick=val[index] +dp[index][cap-wt[index]];
+                }
+                
+            int   notpick=dp[index-1][cap];
+                
+                dp[index][cap]=Math.max(pick,notpick);
+            }
+        }
+        
+        return dp[n-1][capacity];
+    }
+}
+// SPACE OPTIMIZATION
+class Solution {
+    static int knapSack(int val[], int wt[], int capacity) {
+        // code here
+        int n=val.length;
+    
+        int[] prev=new int[capacity+1];
+        for(int cap=0;cap<=capacity;cap++)
+        {
+                prev[cap]=(cap/wt[0]) * val[0];
+        }
+        
+        for(int index=1;index<n;index++)
+        {
+            int[] curr=new int[capacity+1];
+            
+            for(int cap=0;cap<=capacity;cap++)
+            {
+                int pick=(int)-1e9;
+                if(wt[index] <=cap)
+                {
+                    pick=val[index] +curr[cap-wt[index]];
+                }
+                
+               int notpick=prev[cap];
+                
+               curr[cap]=Math.max(pick,notpick);
+            }
+            prev=curr;
+            curr=null;
+        }
+        
+        return prev[capacity];
     }
 }
